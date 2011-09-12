a1aa93d67e8a6fe0379f81f07326839adc4492b2
James William Pye <x@jwp.name>
2011-09-12 15:14:22 -0500

    Register PostgreSQL 9.1 and Python 3.2.

--- src/project.py
+++ src/project.py
@@ -9,14 +9,16 @@ systems = {
 	'pg_8_3': 'PostgreSQL 8.3',
 	'pg_8_4': 'PostgreSQL 8.4',
 	'pg_9_0': 'PostgreSQL 9.0',
+	'pg_9_1': 'PostgreSQL 9.1',
 }
 system_aliases = {
 	'pg_8_5' : 'pg_9_0'
 }
-default_system = 'pg_9_0'
+default_system = 'pg_9_1'
 
 languages = {
 	'py_3_1': 'Python 3.1',
+	'py_3_2': 'Python 3.2',
 }
 
 date = None
