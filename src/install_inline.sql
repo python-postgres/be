2f2690be85e37ba2de86243bb86fc8190124e562
James William Pye <x@jwp.name>
2011-10-05 22:09:41 -0500

    Remove schema creation from script and minor reformatting.

--- src/install_inline.sql
+++ src/install_inline.sql
@@ -1,14 +1,13 @@
 CREATE FUNCTION
- "handler"()
-RETURNS LANGUAGE_HANDLER LANGUAGE C AS 'python', 'pl_handler';
+"handler"
+() RETURNS LANGUAGE_HANDLER LANGUAGE C AS 'python', 'pl_handler';
 
 CREATE FUNCTION
- "validator"(oid)
-RETURNS VOID LANGUAGE C AS 'python', 'pl_validator';
+"validator"
+(oid) RETURNS VOID LANGUAGE C AS 'python', 'pl_validator';
 
 CREATE FUNCTION
- "inline"(INTERNAL)
-RETURNS VOID LANGUAGE C AS 'python', 'pl_inline';
+"inline"
+(INTERNAL) RETURNS VOID LANGUAGE C AS 'python', 'pl_inline';
 
 CREATE LANGUAGE python HANDLER "handler" INLINE "inline" VALIDATOR "validator";
-CREATE SCHEMA __python__;
--- src/install_no_inline.sql
+++ src/install_no_inline.sql
@@ -1,10 +1,9 @@
 CREATE FUNCTION
- "handler"()
-RETURNS LANGUAGE_HANDLER LANGUAGE C AS 'python', 'pl_handler';
+"handler"
+() RETURNS LANGUAGE_HANDLER LANGUAGE C AS 'python', 'pl_handler';
 
 CREATE FUNCTION
- "validator"(oid)
-RETURNS VOID LANGUAGE C AS 'python', 'pl_validator';
+"validator"
+(oid) RETURNS VOID LANGUAGE C AS 'python', 'pl_validator';
 
 CREATE LANGUAGE python HANDLER "handler" VALIDATOR "validator";
-CREATE SCHEMA __python__;
