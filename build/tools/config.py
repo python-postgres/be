c3c1403d0310ef147eea46fc942e5caa392c335d
James William Pye <x@jwp.name>
2011-10-06 10:38:52 -0500

    Learn me some ABIFLAGS.
    Use ABIFLAGS to properly construct link target.

--- build/tools/config.py
+++ build/tools/config.py
@@ -3,10 +3,10 @@ import os
 import os.path
 import uuid
 try:
-	import distutils.sysconfig as sysconfig
+	import sysconfig
 except ImportError:
 	try:
-		import sysconfig
+		import distutils.sysconfig as sysconfig
 	except ImportError:
 		sys.stderr.write("ERROR: no sysconfig module available\n")
 		sys.stderr.write("HINT: Try installing a Python 'dev' package.\n")
@@ -27,35 +27,24 @@ else:
 # build identifier (unused)
 emit('uuid', str(uuid.uuid1()))
 emit('python', sys.executable)
-emit('python_version', str(sys.version_info[0]) + str(sys.version_info[1]))
-emit('python_ref', 'python' + str(sys.version_info[0]) + str(sys.version_info[1]))
 
+pyversion = sysconfig.get_config_var('VERSION')
+emit('python_version', pyversion)
+pyabi = sysconfig.get_config_var('ABIFLAGS') or ''
+emit('python_abi', pyabi)
+
+pyspec = 'python' + pyversion + pyabi
 emit('python_cflags', ' '.join([
-		'-I' + sysconfig.get_python_inc(),
+		'-I' + sysconfig.get_config_var('INCLUDEPY'),
 	]
 ))
 
 emit('project_version', project.version)
 
-version = sysconfig.get_config_vars('VERSION')[0]
-libdir = sysconfig.get_config_vars('LIBDIR')[0]
-
-##
-# XXX: Look for pythonX.Ym
-# The OSX distribution appears to use the 'm' version by default,
-# so if there is no m version in the library directory, assume the
-# sans 'm' library is what we want.
-libpythonm = 'python' + version + 'm'
-for x in os.listdir(libdir):
-	if libpythonm in x:
-		# We found libpythonX.Ym.so in the LIBDIR.
-		libpy = '-l' + libpythonm
-		break
-else:
-	libpy = '-lpython' + version
+libdir = sysconfig.get_config_var('LIBDIR')
+libpy = '-l' + pyspec
 
 emit('python_ldflags', ' '.join([
-		'-L' + libdir,
-		libpy,
+		'-L' + libdir, libpy,
 	] + sysconfig.get_config_vars('SHLIBS', 'SYSLIBS', 'LDFLAGS')
 ))
